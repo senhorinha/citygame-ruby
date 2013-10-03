f00564cf0b2440797cb47e9bc87aebfecd53923c
Thiago Rose <thiago.senhorinha@gmail.com>
2013-10-02 22:20:22 -0300

    Correção de erro de sintaxe

--- src/Tropa.rb
+++ src/Tropa.rb
@@ -11,7 +11,7 @@ class Tropa
   end
 
   def atualiza_forca_de_ataque tecnologia
-    forca_de_ataque = tecnologia * tamanho
+    @forca_de_ataque = tecnologia * tamanho
   end
 
   def ataca tropa_inimiga
@@ -21,11 +21,11 @@ class Tropa
     funcao_cerco = funcao_cerco(forca_de_ataque_inimigo)
 
     #caso_um   : Se a força da tropa de ataque for maior que a força da tropa de defesa
-    caso_um = ((forca_de_ataque/forca_de_ataque_inimigo)**NUMERO_DE_OURO)*(1/funcao_cerco(forca_de_ataque_inimigo))
+    caso_um = ((@forca_de_ataque/forca_de_ataque_inimigo)**NUMERO_DE_OURO)*(1/funcao_cerco(forca_de_ataque_inimigo))
     #caso_dois : Se a força da tropa de defesa for maior que a força da tropa de ataque
     caso_dois = (1/funcao_cerco)
 
-    if(forca_de_ataque > forca_de_ataque_inimigo)
+    if(@forca_de_ataque > forca_de_ataque_inimigo)
       atualiza_valores_pos_batalha caso_um
       tropa_inimiga.atualiza_valores_pos_batalha caso_dois
     else
@@ -39,7 +39,7 @@ class Tropa
   protected
   
   def atualiza_valores_pos_batalha resultado
-    tecnologia_aux = forca_de_ataque/tamanho
+    tecnologia_aux = @forca_de_ataque/tamanho
     tamanho *= resultado
     atualiza_forca_de_ataque(tecnologia_aux)
   end
@@ -47,7 +47,7 @@ class Tropa
   private
 
   def funcao_cerco(forca_de_ataque_inimigo)
-    return Math.sqrt((forca_de_ataque*forca_de_ataque_inimigo)/10*(forca_de_ataque+forca_de_ataque_inimigo))
+    return Math.sqrt((@forca_de_ataque*forca_de_ataque_inimigo)/10*(@forca_de_ataque+forca_de_ataque_inimigo))
   end
 
 end
