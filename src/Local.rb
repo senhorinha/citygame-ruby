b4abcea6fdea2d07574c4173ba425441d6c8cc84
Thiago Rose <thiago.senhorinha@gmail.com>
2013-10-03 17:36:08 -0300

    Adicionando compatibildade UTF-8

--- src/Local.rb
+++ src/Local.rb
@@ -1,3 +1,5 @@
+# -*- encoding : utf-8 -*-
+
 class Local
 
   attr_reader :@tropas
--- src/Tropa.rb
+++ src/Tropa.rb
@@ -1,3 +1,5 @@
+# -*- encoding : utf-8 -*-
+
 class Tropa
 
   attr_reader :local, :tamanho, :jogador, :forca_de_ataque
