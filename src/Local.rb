0587961ecb02a020bbea2662a6e66b4ce3e80f45
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-10-04 16:11:20 -0300

    Dinâmica de movimentação de tropas alterada

--- src/Local.rb
+++ src/Local.rb
@@ -1,57 +1,37 @@
 # -*- encoding : utf-8 -*-
 
 class Local
-  attr_reader :tropas
+  attr_reader :id, :tropas
 
   def initialize id
+    @id = id
     @tropas = []
   end
 
-  # @param [Jogador] jogador
-  def local_disponivel? jogador
-    #TODO: Pensar em estados?
-    if @tropas[0].jogador.eql? tropa.jogador or @tropas[0].nil?
-      true
-    elsif @tropas[1].jogador.eql? tropa.jogador or @tropas[1].nil?
-      true
-    end
-    false
-  end
-
+  # Adiciona a tropa ao local atual, concatenando com tropas amigas
   # @param [Tropa] tropa
-  # @return [TrueClass]
-  # @return [FalseClass]
-  def ocupa tropa
-    #TODO: Resolver esse monte de ifs
-    if local_disponivel? tropa.jogador
-      if @tropas[0].jogador.eql? tropa.jogador
-        @tropas[0].concatena tropa
-      elsif @tropas[1].jogador.eql? tropa.jogador
-        @tropas[1].concatena
-      elsif @tropas[0].nil?
-        @tropas[0] = tropa
-      else
-        @tropas[1] = tropa
+  def ocupar tropa
+    # Checa se existem tropas amigas no local, se sim, mescla as tropas
+    @tropas.each do |outra_tropa|
+      if outra_tropa.jogador == tropa.jogador then
+        outra_tropa.concatenar(tropa)
+        return
       end
-      true
-    else
-      false
     end
+
+    # Se não existem tropas amigas no local, adiciona à lista de tropas
+    @tropas.push tropa
   end
 
+  # Retira a tropa do local atual
   # @param [Tropa] tropa
-  # @return [TrueClass]
-  # @return [FalseClass]
-  def desocupa tropa
-    #TODO: Pensar na utilidade do método
-    if @tropas[0].eql? tropa
-      @tropas[0] = nil
-      true
-    elsif @tropas[1].eql? tropa
-      @tropas[1] = nil
-      true
-    end
-    false
+  def desocupar tropa
+    @tropas.delete tropa
+  end
+
+  # Checa se há tropas inimigas no local. Caso existam, gera as batalhas das tropas, duas a duas
+  def checar_batalhas
+    # TODO: implementar
   end
 
 end
