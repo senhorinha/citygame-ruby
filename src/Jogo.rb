1469352e4c0c08f347924eba7b922d2a3d078b0d
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-10-05 12:49:11 -0300

    Corrigido erro de instanciação do jogador atual em 'passar_a_vez' da classe Jogo

--- src/Jogo.rb
+++ src/Jogo.rb
@@ -41,7 +41,7 @@ class Jogo
     id_jogador_atual = @jogador_atual.id
     id_jogador_atual = id_jogador_atual + 1
     id_jogador_atual = 0 if id_jogador_atual >= @jogadores.size
-    @jogador_atual == @jogadores[id_jogador_atual]
+    @jogador_atual = @jogadores[id_jogador_atual]
   end
 
   def balancear_recursos id_cidade, exercitos, tecnologia
