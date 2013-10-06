f7e4e5319503db19412f0a35d33b596c75900577
Thiago Rose <thiago.senhorinha@gmail.com>
2013-10-05 22:01:40 -0300

    Classe resposável por efetuar o ataque

--- src/Ataque.rb
+++ src/Ataque.rb
@@ -1,3 +1,40 @@
+# -*- encoding : utf-8 -*-
+
+require_relative 'Local'
+require_relative 'Tropa'
+
 class Ataque
-  # To change this template use File | Settings | File Templates.
+
+  attr_reader :local
+  attr_reader :tropa_atacante, :tropa_defensora
+
+  # @param [Local] Local onde esta sendo realizada a batalha
+  # @param [Tropa] tropa_atacante
+  # @param [Tropa] tropa_defensora
+  def initialize local, tropa_atacante, tropa_defensora
+    @local = local
+    @tropa_atacante = tropa_atacante
+    @tropa_defensora = tropa_defensora
+  end
+
+  # @return [FalseClass, TrueClass] retorna false se batalha ainda não acabou
+  def executar
+    if local.is_cidade
+      # Ataque com bônus
+      # Com o parâmetro bonus para facilitar a implementação
+      tropa_defensora.atacar tropa_atacante true
+    else
+      # Ataque sem bônus
+      tropa_atacante.atacar tropa_defensora false
+    end
+    return terminou?
+  end
+
+  private
+
+  # @return [FalseClass,TrueClass] retorna true se batalha acabou
+  def terminou?
+    true if tropa_atacante.tamanho == 0 or tropa_defensora.tamanho == 0 or tropa_atacante.local != tropa_defensora.local
+  end
+
 end
