8967bf9efea2ed605bd3efe3619b45caa7baa504
Thiago Rose <thiago.senhorinha@gmail.com>
2013-10-05 21:58:55 -0300

    Novos métodos adicionar_ataque, executar_ataque e novo atributo FilaDeAtaques

--- src/Jogador.rb
+++ src/Jogador.rb
@@ -1,14 +1,19 @@
 # -*- encoding : utf-8 -*-
+
+require_relative 'FilaDeAtaques'
+
 class Jogador
   attr_reader :id, :nome
   attr_reader :cidades
+  attr_reader :fila_de_ataques
   attr_accessor :tecnologia
 
   def initialize id, nome
-    @id   = id
+    @id = id
     @nome = nome
     @tecnologia = 1
     @cidades = []
+    @fila_de_ataques = FilaDeAtaques
   end
 
   def gerar_recursos
@@ -19,6 +24,17 @@ class Jogador
     end
     tec /= 100
     @tecnologia = tecnologia*(1 + tec)
+  end
+
+  #Deve ser chamado ao final de cada turno para executar todos os ataques que estão na espera
+
+  def adicionar_ataque local, tropa_atacante, tropa_atacante
+    fila_de_ataques.adicionar local, tropa_atacante, tropa_atacante
+  end
+
+  # Método que deve ser executado ao final do turno deste jogador!
+  def executar_ataques
+    fila_de_ataques.executar_ataques
   end
 
 end
