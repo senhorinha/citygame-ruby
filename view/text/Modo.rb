3bc25b8ad8b74ceeba15f187076ef8c507dbeb2e
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-10-07 14:09:50 -0300

    Adicionada documentação à classe Modo da visão modo texto

--- view/text/Modo.rb
+++ view/text/Modo.rb
@@ -1,4 +1,6 @@
 # -*- encoding : utf-8 -*-
+
+# Um modo encapsula uma série de comandos disponíveis em dado momento do jogo
 class Modo
   attr_reader :ativo, :jogo, :comandos
 
@@ -8,14 +10,21 @@ class Modo
     @jogo = jogo
   end
 
+  # String impressa antes de cada chamada de comando do jogador
+  # @return [String]
   def prefixo
     return ' ~> '
   end
 
+  # Processo para o auto completar dos comandos disponíveis
+  # @return [Proc]
   def completion_proc
     return proc { |s| comandos.grep(/^#{s}/) }
   end
 
+  # Submete um comando
+  # @param [Hash] Hash com {:command => 'nome do comando requisitado', :options => 'array com os argumentos'}
+  # @return [Modo] Retorna um novo modo de jogo
   def submeter_comando command_hash
     if !validar_comando(command_hash[:command]) then
       error_msg "'#{command_hash[:command]}' não é um comando válido... Digite 'help' caso esteja perdido!"
@@ -36,6 +45,9 @@ class Modo
     return self
   end
 
+  # Imprime uma mensagem padrão de confirmação
+  # @param [String] Mensagem apresentada
+  # @return [Boolean] True em caso de confirmação
   def confirmar_comando msg
     print msg.colorize(:yellow), " (s, n) "
     c = gets
@@ -43,20 +55,28 @@ class Modo
     return c == 's'
   end
 
+  # Imprime uma mensagem padrão de sucesso
+  # @param [String] Mensagem apresentada
   def success_msg msg
     puts msg.colorize :green
   end
 
+  # Imprime uma mensagem padrão de alerta
+  # @param [String] Mensagem apresentada
   def warning_msg msg
     puts msg.colorize :yellow
   end
 
+  # Imprime uma mensagem padrão de erro
+  # @param [String] Mensagem apresentada
   def error_msg msg
     puts msg.colorize :red
   end
 
 private
 
+  # Checa se o comando é válido dentro do contexto do modo atual
+  # @return [Boolean]
   def validar_comando comando
     return false if !@comandos.include?(comando)
     return false if comando.empty?
@@ -69,6 +89,7 @@ public
   # ========================================================
   # Comandos padrões
 
+  # Encerra o jogo e termina a aplicação
   def exit
     c = confirmar_comando "Encerrar o jogo, perdendo todas as informações?"
     @ativo = !c
