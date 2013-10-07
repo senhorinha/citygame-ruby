b300d9f7ef75d842ac2816f16b8c21d179fb415d
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-10-07 17:02:59 -0300

    Criada classe 'Mapa' para representação do mapa do jogo

--- src/Jogo.rb
+++ src/Jogo.rb
@@ -2,25 +2,17 @@
 require_relative 'exceptions'
 require_relative 'Jogador'
 require_relative 'Cidade'
-require_relative 'Digrafo'
+require_relative 'Mapa'
 
 class Jogo
   attr_reader :jogadores, :jogador_atual
   attr_reader :turno
-  attr_reader :locais # Digrafo
-
-  # Constantes
-  LESTE = 3
-  SUL = 6
-  OESTE = 9
-  NORTE = 12
-  QUANTIDADE_DE_CIDADES = 5
-  QUANTIDADE_DE_CAMPOS = 44
+  attr_reader :mapa
 
   def initialize
     @turno = 0
     @jogadores = []
-    @locais = criar_locais
+    @mapa = Mapa.new
   end
 
   # Cria um novo jogador
@@ -36,6 +28,9 @@ class Jogo
   # Inicia a partida, passando a vez para o primeiro jogador
   def iniciar
     raise MinimoDeJogadoresException, 'Pelo menos dois jogadores devem existir para uma partida acontecer!' if @jogadores.size < 2
+
+    @mapa.criar_locais
+    # TODO: atribuir cidades aos jogadores
 
     @turno = 1
     @jogador_atual = @jogadores[rand(jogadores.size)]
@@ -66,19 +61,18 @@ class Jogo
   # Movimenta tropa com n_soldados do jogador_atual, a tropa parte do local cujo id é
   # id_fonte e segue na direcao estabelecida um Local de distância
   def movimentar_tropas id_fonte, n_soldados, direcao
-    if(direcao!=LESTE and direcao!=SUL and direcao!=OESTE and direcao!=NORTE)
-      raise DirecaoException, "Direção não pertence a {LESTE, SUL, OESTE, NORTE}"
-    end
-      raise NumeroDeExercitosException, "Número de soldados inválido" if n_soldados < 1
+    raise DirecaoException, "Direção não pertence a {LESTE, SUL, OESTE, NORTE}" unless @mapa.direcao_valida? direcao
 
-    for vertice in locais.vertices
+    raise NumeroDeExercitosException, "Número de soldados inválido" if n_soldados < 1
+
+    for vertice in @mapa.grafo.vertices
       if(vertice.id == id_fonte and vertice.jogador == @jogador_atual)
         tropa_selecionada = nil
         for tropa in vertice.tropas
           tropa_selecionada = tropa if tropa.jogador == @jogador_atual
         end
         raise NumeroDeExercitosException, "Número de soldados maior que o existente" if n_soldados > tropa_selecionada.tamanho
-        for sucessor in locais.sucessores(vertice)
+        for sucessor in @mapa.grafo.sucessores(vertice)
           vertice_destino, d = sucessor.v, sucessor.peso
           sucesso = tropa_selecionada.movimentar(n_soldados, vertice_destino) if d == direcao
           return vertice_destino if sucesso
@@ -92,81 +86,8 @@ class Jogo
   # Recebe uma string {NORTE, SUL, LESTE, OESTE} e retorna um inteiro que representa a direção informada
   # @param [String] Um dentre {NORTE, SUL, LESTE, OESTE}
   # @return [Integer] Int que representa a direção ou nil em caso de direção não reconhecida
-  def direcao str_direction
-    direcao = nil
-
-    case str_direction.upcase
-      when 'NORTE'
-        direcao = NORTE
-      when 'SUL'
-        direcao = SUL
-      when 'LESTE'
-        direcao = LESTE
-      when 'OESTE'
-        direcao = OESTE
-    end
-
-    return direcao
-  end
-
-private
-
-  # Cria todos os locais do jogo como um tabuleiro virtual quadrado
-  # utilizando um digrafo G(V, A)
-  # V = {v | v e um local}
-  # A = {(v, w, p) | v, w pertencem a V tal que v é adjacente a w e
-  #                  p pertence a {LESTE, SUL, OESTE, NORTE} tal que ele representa
-  #                  a posicao de w em relacao a v}
-  def criar_locais
-    # Criar locais
-    n_loc = QUANTIDADE_DE_CIDADES + QUANTIDADE_DE_CAMPOS
-    raise LocalException, 'Não existem locais suficientes para iniciar o jogo' if (n_loc**(0.5))%(n_loc**(0.5)).floor != 0
-    loc = Array.new(n_loc)
-    for i in 0...QUANTIDADE_DE_CIDADES
-      loc[i] = Cidade.new(i,true)
-    end
-    for i in 0...QUANTIDADE_DE_CAMPOS
-      j = (QUANTIDADE_DE_CIDADES + i)
-      loc[j] = Local.new(j,false)
-    end
-
-    # Misturar locais
-    for i in 0...n_loc
-      destino_randomico = rand(n_loc)
-      loc[i], loc[destino_randomico] = loc[destino_randomico], loc[i]
-    end
-
-    # Criar grafo que representa os locais do jogo
-    # Exemplo de mapa criado para auxiliar a criacao do grafo:
-    #      local(04)--local(08)--local(10)-- ... --local(56)
-    #         |          |           |                 |
-    #      local(01)--local(03)--local(02)-- ... --local(34)
-    #         |          |           |     ..           |
-    #         :          :           :        ..        :
-    #         |          |           |           ..     |
-    #      local(07)--local(65)--local(79)-- ... --local(81)
-    k = 0
-    largura = n_loc**(0.5)
-    comprimento = largura
-    mapa = Array.new(comprimento) { |linha| linha = Array.new(largura) }
-    grafo = Digrafo.new
-    for i in 0...comprimento
-      for j in 0...largura
-        mapa[i][j] = loc[k]
-        grafo.adicionar_vertice(mapa[i][j])
-        unless (j-1) < 0
-          grafo.conectar(mapa[i][j], mapa[i][j-1], OESTE)
-          grafo.conectar(mapa[i][j-1], mapa[i][j], LESTE)
-        end
-        unless (i-1) < 0
-          grafo.conectar(mapa[i][j], mapa[i-1][j], NORTE)
-          grafo.conectar(mapa[i-1][j], mapa[i][j], SUL)
-        end
-        k = k + 1
-      end
-    end
-
-    return grafo
+  def direcao str_dir
+    return @mapa.str_direcao str_dir
   end
 
 end
---
+++ src/Mapa.rb
@@ -0,0 +1,106 @@
+# -*- encoding : utf-8 -*-
+
+require_relative 'exceptions'
+require_relative 'Digrafo'
+
+class Mapa
+  attr_reader :campos, :cidades, :matriz, :grafo
+
+  # Constantes
+  LESTE = 3
+  SUL = 6
+  OESTE = 9
+  NORTE = 12
+  QUANTIDADE_DE_CIDADES = 5
+  QUANTIDADE_DE_CAMPOS = 44
+
+  # Cria um novo mapa aleatório
+  def initialize
+    @campos = []
+    @cidades = []
+    @matriz = [[]]
+  end
+
+  # Checa se o id da direção é uma direção válida
+  # @param [Integer] Constante que representa uma direção
+  # @return [Boolean]
+  def direcao_valida? direcao_const
+    direcao_const == LESTE or direcao_const == SUL or direcao_const == OESTE or direcao_const == NORTE
+  end
+
+  # Recebe uma string {NORTE, SUL, LESTE, OESTE} e retorna um inteiro que representa a direção informada
+  # @param [String] Um dentre {NORTE, SUL, LESTE, OESTE}
+  # @return [Integer] Int que representa a direção ou nil em caso de direção não reconhecida
+  def str_direcao str_dir
+    direcao = nil
+
+    case str_dir.upcase
+      when 'NORTE'
+        direcao = NORTE
+      when 'SUL'
+        direcao = SUL
+      when 'LESTE'
+        direcao = LESTE
+      when 'OESTE'
+        direcao = OESTE
+    end
+
+    return direcao
+  end
+
+  def criar_locais
+    # Criar locais
+    n_loc = QUANTIDADE_DE_CIDADES + QUANTIDADE_DE_CAMPOS
+    raise LocalException, 'Não existem locais suficientes para iniciar o jogo' if (n_loc**(0.5))%(n_loc**(0.5)).floor != 0
+    loc = Array.new(n_loc)
+    for i in 0...QUANTIDADE_DE_CIDADES
+      loc[i] = Cidade.new(i,true)
+      @cidades.push loc[i]
+    end
+    for i in 0...QUANTIDADE_DE_CAMPOS
+      j = (QUANTIDADE_DE_CIDADES + i)
+      loc[j] = Local.new(j,false)
+      @campos.push loc[j]
+    end
+
+    # Misturar locais
+    for i in 0...n_loc
+      destino_randomico = rand(n_loc)
+      loc[i], loc[destino_randomico] = loc[destino_randomico], loc[i]
+    end
+
+    # Criar grafo que representa os locais do jogo
+    # Exemplo de mapa criado para auxiliar a criacao do grafo:
+    #      local(04)--local(08)--local(10)-- ... --local(56)
+    #         |          |           |                 |
+    #      local(01)--local(03)--local(02)-- ... --local(34)
+    #         |          |           |     ..           |
+    #         :          :           :        ..        :
+    #         |          |           |           ..     |
+    #      local(07)--local(65)--local(79)-- ... --local(81)
+    k = 0
+    largura = n_loc**(0.5)
+    comprimento = largura
+    mapa = Array.new(comprimento) { |linha| linha = Array.new(largura) }
+    grafo = Digrafo.new
+    for i in 0...comprimento
+      for j in 0...largura
+        mapa[i][j] = loc[k]
+        grafo.adicionar_vertice(mapa[i][j])
+        unless (j-1) < 0
+          grafo.conectar(mapa[i][j], mapa[i][j-1], OESTE)
+          grafo.conectar(mapa[i][j-1], mapa[i][j], LESTE)
+        end
+        unless (i-1) < 0
+          grafo.conectar(mapa[i][j], mapa[i-1][j], NORTE)
+          grafo.conectar(mapa[i-1][j], mapa[i][j], SUL)
+        end
+        k = k + 1
+      end
+    end
+
+    @matriz = mapa
+    @grafo = grafo
+  end
+
+end
