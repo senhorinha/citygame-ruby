71392b3e7f21cb6546838c4d018dd2e16b9cc99a
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-10-07 13:57:53 -0300

    Adicionada documentação à classe CommandParser da visão modo texto

--- view/text/CommandParser.rb
+++ view/text/CommandParser.rb
@@ -1,6 +1,12 @@
 # -*- encoding : utf-8 -*-
+
+# Utilizada para interpretar os comandos digitados na linha de comando
 class CommandParser
 
+  # Interpreta um comando digitado pelo jogador
+  # Os comandos são da forma `nome_do_comando argumento1, argumento2, argumento3, ...`
+  # @param [String] String digitada
+  # @return [Hash] { :command => 'nome do comando digitado', :options => 'array dos argumentos' }
   def parse string
     pieces = string.split ' ', 2
     command = pieces[0]
