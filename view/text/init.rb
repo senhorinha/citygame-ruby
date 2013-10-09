5d5974c8ce0dedf663e2fce330c29a3fd41ed506
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-10-09 09:36:48 -0300

    Banner do jogo colorizado (modo texto)

--- view/text/init.rb
+++ view/text/init.rb
@@ -5,22 +5,24 @@ require_relative '../../src/Jogo'
 require_relative 'CommandParser'
 require_relative 'ModoNovoJogo'
 
-puts ""
-puts " ______     __     ______   __  __     ______     ______     __    __     ______  "
-puts "/\\  ___\\   /\\ \\   /\\__  _\\ /\\ \\_\\ \\   /\\  ___\\   /\\  __ \\   /\\ \"-./  \\   /\\  ___\\   "
-puts "\\ \\ \\____  \\ \\ \\  \\/_/\\ \\/ \\ \\____ \\  \\ \\ \\__ \\  \\ \\  __ \\  \\ \\ \\-./\\ \\  \\ \\  __\\   "
-puts " \\ \\_____\\  \\ \\_\\    \\ \\_\\  \\/\\_____\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\ "
-puts "  \\/_____/   \\/_/     \\/_/   \\/_____/   \\/_____/   \\/_/\\/_/   \\/_/  \\/_/   \\/_____/ "
-puts ""
-puts ""
+jogo = Jogo.new
+parser = CommandParser.new
+modo = ModoNovoJogo.new jogo # Modo inicial
+
+modo.clear
+
+puts "".colorize :color => :light_cyan, :mode => :bold
+puts " ______     __     ______   __  __     ______     ______     __    __     ______  ".colorize :color => :light_cyan, :mode => :bold
+puts "/\\  ___\\   /\\ \\   /\\__  _\\ /\\ \\_\\ \\   /\\  ___\\   /\\  __ \\   /\\ \"-./  \\   /\\  ___\\   ".colorize :color => :light_cyan, :mode => :bold
+puts "\\ \\ \\____  \\ \\ \\  \\/_/\\ \\/ \\ \\____ \\  \\ \\ \\__ \\  \\ \\  __ \\  \\ \\ \\-./\\ \\  \\ \\  __\\   ".colorize :color => :light_cyan, :mode => :bold
+puts " \\ \\_____\\  \\ \\_\\    \\ \\_\\  \\/\\_____\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\ ".colorize :color => :light_cyan, :mode => :bold
+puts "  \\/_____/   \\/_/     \\/_/   \\/_____/   \\/_____/   \\/_/\\/_/   \\/_/  \\/_/   \\/_____/ ".colorize :color => :light_cyan, :mode => :bold
+puts "".colorize :color => :light_cyan, :mode => :bold
+puts "".colorize :color => :light_cyan, :mode => :bold
 
 puts "Digite 'help' a qualquer momento para visualizar os comandos disponíveis"
 puts "Chame seus amigos e divirta-se :)"
 puts ""
-
-jogo = Jogo.new
-parser = CommandParser.new
-modo = ModoNovoJogo.new jogo # Modo inicial
 
 Readline.completion_append_character = ' ' # Caracter impresso após uma chamada de auto completar
 
