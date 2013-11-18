ae95662ac815a35e24241052512eec96c6f1b92e
tonussi <lptonussi@gmail.com>
2013-11-18 15:47:35 -0200

    Add references oldstyle for easy understandings

--- db/script-table.sql
+++ db/script-table.sql
@@ -1,18 +1,27 @@
 -- Script: Tabelas
 -- Learn: http://www.postgresql.org/docs/current/static/sql-createtable.html
 
-CREATE TABLE usuarios (
+CREATE TABLE USUARIOS (
     username VARCHAR(30) PRIMARY KEY,
     password VARCHAR(30)
 );
 
-CREATE TABLE batalha_usuario (
-    username VARCHAR(30) PRIMARY KEY FOREIGN KEY,
-    password VARCHAR(30) PRIMARY KEY FOREIGN KEY
+CREATE TABLE BATALHA_USUARIO (
+    username VARCHAR(30),
+    batalha_id INT,
+    PRIMARY KEY (username, password),
+    FOREIGN KEY batalha_id REFERENCES BATALHAS(id)
+    FOREIGN KEY username REFERENCES USUARIOS(username)
 );
 
-CREATE TABLE batalha_usuario (
-    id INT PRIMARY KEY,
+CREATE TABLE BATALHAS (
+    id INT,
     turnos INT,
-    vencedor VARCHAR(30) FOREIGN KEY
+    vencedor VARCHAR(30),
+    PRIMARY KEY (id),
+    FOREIGN KEY vencedor REFERENCES USUARIOS(username)
 );
+
+delete from USUARIOS;
+delete from BATALHA_USUARIO;
+delete from BATALHAS;
