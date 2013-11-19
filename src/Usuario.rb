8ed5776c2d9b3aff084cc5c5a2ff28ad80cef79f
Carlos Bonetti <carlosb_bc@hotmail.com>
2013-11-19 18:18:05 -0200

    Criados métodos de construção e validação para classe Usuario

--- src/Usuario.rb
+++ src/Usuario.rb
@@ -1,13 +1,32 @@
 # -*- encoding : utf-8 -*-
 
+require 'digest'
 require_relative 'LogBatalha'
 
 class Usuario
-  attr_reader :username, :password, :password_confirm
+  attr_reader :username, :password
   attr_reader :batalhas
 
-  def initialize
+  def initialize username, password, password_confirm
+    raise ArgumentError, "Nome de usuário deve conter 3-30 caraceres (somente letras, números e _)" unless Usuario.valid_username?(username)
+    @username = username
 
+    raise ArgumentError, "Senha deve conter 4-30 caracteres" unless Usuario.valid_password?(password)
+    raise ArgumentError, "A senha e sua confirmação não são idênticas" unless password == password_confirm
+
+    @password = Usuario.digest password
+  end
+
+  def self.valid_username? username
+    !username.match(/^[a-zA-Z0-9_]{3,30}$/).nil?
+  end
+
+  def self.valid_password? password
+    !password.match(/^.{4,30}$/).nil?
+  end
+
+  def self.digest password
+    Digest::SHA1.hexdigest(password)
   end
 
 end
--- tests/UsuarioTest.rb
+++ tests/UsuarioTest.rb
@@ -5,12 +5,62 @@ require_relative '../src/Usuario'
 
 class UsuarioTest < Test::Unit::TestCase
 
-  def setup
-    usuario = Usuario.new
+  def testar_validacao_de_username
+    assert_equal true, Usuario.valid_username?('napoleao')
+    assert_equal true, Usuario.valid_username?('Napoleao')
+    assert_equal true, Usuario.valid_username?('Napoleao123')
+    assert_equal true, Usuario.valid_username?('napoleao_bonaparte')
+    assert_equal true, Usuario.valid_username?('nap')
+    assert_equal false, Usuario.valid_username?('na')
+    assert_equal false, Usuario.valid_username?('n')
+    assert_equal false, Usuario.valid_username?('')
+    assert_equal false, Usuario.valid_username?('napoleão')
+    assert_equal false, Usuario.valid_username?('napoleao bonaparte')
+    assert_equal false, Usuario.valid_username?('napo!')
+    assert_equal false, Usuario.valid_username?('nome_com_mais_de_30_caracteres_para_teste_de_tamanho')
   end
 
-  def testar_vazio
-    assert 1
+  def testar_criacao_com_username_invalido
+    assert_raise ArgumentError do
+      Usuario.new 'Napoleão1', 'r31d4guerr4', 'r31d4guerr4'
+    end
+    Usuario.new 'napoleao', 'r31d4guerr4', 'r31d4guerr4'
+  end
+
+  def testar_validacao_de_password
+    assert_equal true, Usuario.valid_password?('1234')
+    assert_equal true, Usuario.valid_password?('r31d4guerr4')
+    assert_equal true, Usuario.valid_password?('caracteres estranhos !!! @,')
+    assert_equal false, Usuario.valid_password?('')
+    assert_equal false, Usuario.valid_password?('1')
+    assert_equal false, Usuario.valid_password?('12')
+    assert_equal false, Usuario.valid_password?('123')
+    assert_equal false, Usuario.valid_password?('senha com mais de 30 caracteres para teste de tamanho')
+  end
+
+  def testar_criacao_com_senha_invalida
+    assert_raise ArgumentError do
+      Usuario.new 'napoleao', '12', '12'
+    end
+    Usuario.new 'napoleao', 'r31d4guerr4', 'r31d4guerr4'
+  end
+
+  def testar_criacao_com_senha_e_confirmacao_diferentes
+    assert_raise ArgumentError do
+      Usuario.new 'napoleao', '1234', '12345'
+    end
+    Usuario.new 'napoleao', '1234', '1234'
+  end
+
+  def testar_se_password_foi_criptografado
+    user = Usuario.new 'cesar', 'daiacesaroqueehdecesar', 'daiacesaroqueehdecesar'
+    assert_not_equal 'daiacesaroqueehdecesar', user.password
+  end
+
+  def testar_criacao
+    user = Usuario.new 'leonidas', 'raul', 'raul'
+    assert_equal 'leonidas', user.username
+    assert_equal false, user.password.nil?
   end
 
 end
